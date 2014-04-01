a2101011282eaa0654fe0c5c944385b1aaa1b53a
miguel.serrano <miguel.serrano@plumbee.co.uk>
2014-04-01 15:25:46 +0100

    added timeout and retry for socket connection in integration tests

--- java-lib/src/test/java/flashdriver/integration/IntegrationTestBase.java
+++ java-lib/src/test/java/flashdriver/integration/IntegrationTestBase.java
@@ -3,6 +3,8 @@ package flashdriver.integration;
 import flashdriver.FlashDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Rule;
+import org.junit.rules.Timeout;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.io.IOException;
@@ -15,12 +17,22 @@ public class IntegrationTestBase {
 
     protected FlashDriver flashDriver;
 
+    @Rule
+    public Timeout globalTimeout = new Timeout(25000);
+
     @Before
     public void openBrowser() throws Exception {
         driver = new FirefoxDriver();
         driver.get(TEST_APP);
-        flashDriver = new FlashDriver();
-        flashDriver.connect();
+        while(flashDriver == null)
+        {
+            try {
+                flashDriver = new FlashDriver();
+                flashDriver.connect();
+            } catch(Exception e) {
+                flashDriver = null;
+            }
+        }
     }
 
     @After
